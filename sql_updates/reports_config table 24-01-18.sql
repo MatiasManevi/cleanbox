498951f85c79591c68e87e53f3434211b56743b9
matiasmanevi <manevimatias@gmail.com>
2018-05-07 19:02:23 -0300

    FIx sql and tranfer

--- sql_updates/reports_config table 24-01-18.sql
+++ sql_updates/reports_config table 24-01-18.sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.3.12
--- http://www.phpmyadmin.net
---
--- Host: localhost:3306
--- Generation Time: Jan 24, 2018 at 04:10 PM
--- Server version: 10.0.17-MariaDB
--- PHP Version: 5.6.7
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `cleanbox`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `reports_config`
 --
@@ -61,6 +37,4 @@ ALTER TABLE `reports_config`
 --
 ALTER TABLE `reports_config`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
